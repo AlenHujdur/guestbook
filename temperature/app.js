c0cc94ef157dd41805ec9be440efcb063ee6705c
AlenHujdur <hujduralen@gmail.com>
2018-06-18 23:49:09 +0200

    adding params

--- temperature/app.js
+++ temperature/app.js
@@ -5,3 +5,34 @@ var ForecastIo = require('forecastio');
 
 var app = express();
 var weather = new ForecastIo("2ec9b1fa287a1af511372484308aa326");
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.set("views", path.resolve(__dirname, "public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+app.get(/^\/(\d{5})$/, function(req, res, next) {
+  var zipcode = req.params[0];
+  var location = zipdb.zipcode(zipcode);
+  if (!location.zipcode) {
+    next();
+  return;
+}
+
+var latitude = location.latitude;
+var longitude = location.longitude;
+weather.forecast(latitude, longitude, function(err, data) {
+  if (err) {
+    next();
+  return;
+}
+
+res.json({
+    zipcode: zipcode,
+    temperature: data.currently.temperature});
+  });
+});
