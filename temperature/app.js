e74ae665214c33968f892a26cc0746acbc370cff
AlenHujdur <hujduralen@gmail.com>
2018-06-27 23:59:25 +0200

    app js commented

--- temperature/app.js
+++ temperature/app.js
@@ -1,10 +1,10 @@
-var path = require('path');
-var express = require('express');
+// var path = require('path');
+// var express = require('express');
 //var zipdb = require('zippity-do-dah');
 //var ForecastIo = require('forecast.io');
 //'use strict';
 //const ForecastIo = require('forecast-io');
-var zipdb = require("zippity-do-dah");
+// var zipdb = require("zippity-do-dah");
 // var ForecastIo = require("forecastio");
 // var app = express();
 // var weather = new ForecastIo('2ec9b1fa287a1af511372484308aa326');
